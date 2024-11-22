02482416a7f53374265b1447c359667946198e32
joeljoanlopez <joeljoanlopez@gmail.com>
2024-11-22 18:40:15 +0100

    Added Jump and Goomba

---
+++ Assets/Scripts/Classes/GoombaController.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class GoombaController : MonoBehaviour, RestartGameElement
+{
+    CharacterController characterController;
+    Vector3 startingPosition;
+    Quaternion startingRotation;
+
+    private void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+        startingPosition = transform.position;
+        startingRotation = transform.rotation;
+        GameManager.GetGameManager().AddRestartGameElement(this);
+    }
+
+    public void RestartGame()
+    {
+        gameObject.SetActive(true);
+        characterController.enabled = false;
+        transform.position = startingPosition;
+        transform.rotation = startingRotation;
+        characterController.enabled = true;
+    }
+
+    public void Kill()
+    {
+        gameObject.SetActive(false);
+    }
+}
--- Assets/Scripts/Classes/MarioController.cs
+++ Assets/Scripts/Classes/MarioController.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using JetBrains.Annotations;
 using UnityEngine;
 
@@ -13,12 +14,21 @@ public class MarioController : MonoBehaviour, RestartGameElement
     public float rotationSpeed = 1.0f;
     float verticalSpeed = 0.0f;
 
+    [Header("Jump")]
+    public float jumpVerticalSpeed = 5.0f;
+    public float killJumpVerticalSpeed = 0.5f;
+    public float waitStartJumpTime = 0.12f;
+    public float maxAngleNeededToKill = 15.0f;
+    public float minVerticalSpeedToKill = 1.0f;
+    public float fallingVerticalSpeedMultiplier = 0.1f;
+
     [Header("Input")]
     public KeyCode leftKeyCode = KeyCode.A;
     public KeyCode rightKeyCode = KeyCode.D;
     public KeyCode upKeyCode = KeyCode.W;
     public KeyCode downKeyCode = KeyCode.D;
     public KeyCode runKeyCode = KeyCode.LeftShift;
+    public KeyCode jumpKeyCode = KeyCode.Space;
 
     // Game manager vars
     private Vector3 startingPosition;
@@ -90,17 +100,48 @@ public class MarioController : MonoBehaviour, RestartGameElement
             animator.SetFloat("Speed", 0.0f);
         }
 
+        if (CanJump() && Input.GetKeyDown(jumpKeyCode))
+        {
+            Jump();
+        }
 
         movement = movement * speed * Time.deltaTime;
         verticalSpeed += Physics.gravity.y * Time.deltaTime;
-        movement.y = verticalSpeed;
+        movement.y = verticalSpeed * Time.deltaTime;
 
         CollisionFlags collisionFlags = characterController.Move(movement);
-        if ((collisionFlags & CollisionFlags.Below) != 0 || (collisionFlags & CollisionFlags.Above) != 0 && verticalSpeed > 0.0f)
+        if ((collisionFlags & CollisionFlags.Below) != 0 && verticalSpeed < 0.0f)
+        {
+            animator.SetBool("falling", false);
+        }
+        else
+        {
+            animator.SetBool("falling", true);
+        }
+        if ((collisionFlags & CollisionFlags.Below) != 0 && (verticalSpeed < 0.0f) || (collisionFlags & CollisionFlags.Above) != 0 && (verticalSpeed > 0.0f))
         {
             verticalSpeed = 0;
         }
 
+    }
+
+    private bool CanJump()
+    {
+        //TODO fix this
+        return true;
+    }
+
+    private void Jump()
+    {
+        animator.SetTrigger("Jump");
+        StartCoroutine(ExecuteJump());
+    }
+
+    IEnumerator ExecuteJump()
+    {
+        yield return new WaitForSeconds(waitStartJumpTime);
+        verticalSpeed = jumpVerticalSpeed;
+        animator.SetBool("falling", false);
     }
 
     public void SetCheckpoint(CheckpointController newCheckpoint)
@@ -122,5 +163,34 @@ public class MarioController : MonoBehaviour, RestartGameElement
             transform.rotation = currentCheckpoint.respawnPoint.rotation;
         }
         characterController.enabled = true;
+    }
+
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        if (hit.gameObject.CompareTag("Goomba"))
+        {
+            if (IsUpperHit(hit.transform))
+            {
+                hit.gameObject.GetComponent<GoombaController>().Kill();
+                verticalSpeed = killJumpVerticalSpeed;
+            }
+            else
+            {
+                //TODO test take damage
+                //TODO Separar player i goomba
+                GetComponent<HealthController>().RemoveHealth(10);
+            }
+        }
+    }
+
+    private bool IsUpperHit(Transform enemy)
+    {
+        Vector3 enemyDirection = transform.position - enemy.position;
+        enemyDirection.Normalize();
+
+        float dotAngle = Vector3.Dot(enemyDirection, Vector3.up);
+        if (dotAngle >= Mathf.Cos(maxAngleNeededToKill * Mathf.Deg2Rad) && verticalSpeed <= minVerticalSpeedToKill)
+            return true;
+        return false;
     }
 }
